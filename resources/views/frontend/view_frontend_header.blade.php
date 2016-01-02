7acc6fdb0fde4626148bd6883764ee244a698000
Kristian Indra <kristian.indra.1412@gmail.com>
2016-01-02 20:59:40 +0700

    Error, pls check

--- resources/views/frontend/view_frontend_header.blade.php
+++ resources/views/frontend/view_frontend_header.blade.php
@@ -7,8 +7,18 @@
         </td>
         <td align="right">
           <ul id="user">
+          {{ $member_id = auth()->guard('member')->user()->member_id }}
+          @if ($member_id != '')            
+            <li><a href="{{ url('/login') }}">
+              {{ "ASD" }}
+            </a></li>
+            <li><a href="{{ url('/register') }}">
+              Logout
+            </a></li>
+          @else
             <li><a href="{{ url('/login') }}">Login</a></li>
             <li><a href="{{ url('/register') }}">Register</a></li>
+          @endif
           </ul>
         </td>
       </tr>
